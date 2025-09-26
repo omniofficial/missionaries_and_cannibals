e260d83d5e8794bf9dd43ad17c416c01f5a7ecf5
omniofficial <omniofficial04@gmail.com>
2025-09-26 15:25:54 -0700

    Added full search.py file

--- misscannibalsvariant.py
+++ misscannibalsvariant.py
@@ -1,16 +1,21 @@
 from search import *
+
+
 class MissCannibalsVariant(Problem):
 
-def __init__(self, N1=4, N2=4, goal=(0, 0, False)):
+    def __init__(self, N1=4, N2=4, goal=(0, 0, False)):
+        """Define goal state and initialize a problem"""
+        initial = (N1, N2, True)
+        self.N1 = N1
+        self.N2 = N2
+        super().__init__(initial, goal)
 
-initial = (N1, N2, True)
-self.N1 = N1
-self.N2 = N2
-super().__init__(initial, goal)
-if __name__ == '__main__':
-mc = MissCannibalsVariant(4,4)
 
-path = depth_first_graph_search(mc).solution()
-print(path)
-path = breadth_first_graph_search(mc).solution()
-print(path)
+if __name__ == "__main__":
+    mc = MissCannibalsVariant(4, 4)
+    # print(mc.actions((3, 3, True))) # Test your code as you develop! This should return  ['MC', 'MMM']
+
+    path = depth_first_graph_search(mc).solution()
+    print(path)
+    path = breadth_first_graph_search(mc).solution()
+    print(path)
