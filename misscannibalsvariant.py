7f1c046d60353a5d9a2ae30f3cd24294ba9a45d5
omniofficial <omniofficial04@gmail.com>
2025-09-26 15:21:03 -0700

    Added starter code

---
+++ misscannibalsvariant.py
@@ -0,0 +1,16 @@
+from search import *
+class MissCannibalsVariant(Problem):
+
+def __init__(self, N1=4, N2=4, goal=(0, 0, False)):
+
+initial = (N1, N2, True)
+self.N1 = N1
+self.N2 = N2
+super().__init__(initial, goal)
+if __name__ == '__main__':
+mc = MissCannibalsVariant(4,4)
+
+path = depth_first_graph_search(mc).solution()
+print(path)
+path = breadth_first_graph_search(mc).solution()
+print(path)
